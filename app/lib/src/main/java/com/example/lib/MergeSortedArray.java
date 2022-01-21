ec4a4e4bd472207494dffe4b97b1d8acfe198394
mohitkumar0812 <95613405+mohitkumar0812@users.noreply.github.com>
2022-01-21 18:59:58 +0530

    add new

---
+++ app/lib/src/main/java/com/example/lib/MergeSortedArray.java
@@ -0,0 +1,41 @@
+package com.example.lib;
+
+/**
+ * @author Mohit Kumar
+ * @created 21/01/22}
+ */
+public class MergeSortedArray
+{
+    public static void main(String ar[])
+    {
+
+        int nums1[] = new int[]{1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = new int[]{2, 5, 6};
+        int n = 3;
+        merge(nums1, m, nums2, n);
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n)
+    {
+
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (j >= 0)
+        {
+
+            if (i < 0 || nums2[j] >= nums1[i])
+            {
+                nums1[k--] = nums2[j--];
+
+            } else
+            {
+                nums1[k--] = nums1[i--];
+            }
+
+        }
+
+    }
+}
