3289085a76bc19134549eb99827b2d52e49095d5
Mohit KumaR <mohitk@zendrive.com>
2022-01-24 19:12:27 +0530

    add

--- app/lib/src/main/java/com/example/lib/MinimumWindowSubstring.java
+++ app/lib/src/main/java/com/example/lib/MinimumWindowSubstring.java
@@ -5,18 +5,32 @@ public class MinimumWindowSubstring {
     public static void main(String[] args) {
         String input = "ADOBECODEBANC";
         String t = "ABC";
-        int startIndex = 0;
-        String te = "";
-        char[] in = new char[52];
-        for (char ch : t.toCharArray()) {
-            in[ch]++;
+
+        int[] ch = new int[128];
+        String res = "";
+
+        for(char c: t.toCharArray()) {
+            ch[c]++;
         }
-        System.out.println("dw");
 
-        for (int endIndex = 0; endIndex < input.length(); endIndex++) {
+        int windowStart = 0, matched = 0;
+        for (int windowEnd = 0; windowEnd < input.length(); ++windowEnd) {
+            char right = input.charAt(windowEnd);
+            if (--ch[right] >= 0) matched++;
 
+            while (matched == t.length()) {
+                if (res.isEmpty() || windowEnd - windowStart + 1 < res.length()) {
+                    res = input.substring(windowStart, windowEnd + 1);
+                }
 
+                char left = input.charAt(windowStart);
+                if (++ch[left] > 0) {
+                    matched--;
+                }
+                windowStart++;
+            }
         }
+        System.out.println(res);
 
 
     }
