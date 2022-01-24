0de83db8caf2382ea4505fa93877ca4d36fa5d51
Mohit KumaR <mohitk@zendrive.com>
2022-01-24 18:32:14 +0530

    add

---
+++ app/lib/src/main/java/com/example/lib/MinimumWindowSubstring.java
@@ -0,0 +1,23 @@
+package com.example.lib;
+
+public class MinimumWindowSubstring {
+    // Driver code
+    public static void main(String[] args) {
+        String input = "ADOBECODEBANC";
+        String t = "ABC";
+        int startIndex = 0;
+        String te = "";
+        char[] in = new char[52];
+        for (char ch : t.toCharArray()) {
+            in[ch]++;
+        }
+        System.out.println("dw");
+
+        for (int endIndex = 0; endIndex < input.length(); endIndex++) {
+
+
+        }
+
+
+    }
+}
